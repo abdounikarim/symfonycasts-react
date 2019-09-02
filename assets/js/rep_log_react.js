f4107468dea024183e47e933e3047a83ee7e8ac1
abdounikarim <abdounikarim@gmail.com>
2019-09-02 10:03:47 +0200

    React Components

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import { render } from 'react-dom';
+import RepLogApp from "./RepLog/RepLogApp";
 
-const el = <h2>Lift Stuff ! <span>❤️</span></h2>;
-
-ReactDom.render(el, document.getElementById('lift-stuff-app'));
+render(<RepLogApp/>, document.getElementById('lift-stuff-app'));
