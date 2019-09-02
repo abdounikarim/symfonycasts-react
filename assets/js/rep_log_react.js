4f3a77cbf592e0b32e4d1a0d0fa218732c282569
abdounikarim <abdounikarim@gmail.com>
2019-09-02 09:54:03 +0200

    JSX

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const el = React.createElement(
-    'h2',
-    null,
-    'Lift History!',
-    React.createElement('span', null, '❤️')
-);
+const el = <h2>Lift Stuff ! <span>❤️</span></h2>;
 
 ReactDom.render(el, document.getElementById('lift-stuff-app'));
